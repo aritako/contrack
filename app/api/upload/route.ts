c05db871b33e8aa3cc68bf7a14169b47c5ba5dd9
aritako <atienzabusiness@gmail.com>
2024-11-07 11:00:00 +0800

    feat: sample upload

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,5 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { UploadRequestModel } from './types';
+export async function POST(request: NextRequest) {
+  const formData: FormData = await request.formData();
+  const uploadRequest: UploadRequestModel = {
+    file: formData.get('file') as File,
+  };
 
-export async function POST(){
-  return NextResponse.json({ message: 'Hello, World!' });
+  if(!uploadRequest.file) {
+    return NextResponse.json(
+      {error: "No files received."},
+      {status: 400}
+    );
+  }
+  const buffer = Buffer.from(await uploadRequest.file.arrayBuffer());
+  const filename = uploadRequest.file.name;
+  return NextResponse.json(
+    { message: filename },
+    { status: 200 }
+  );
 }
---
+++ app/api/upload/types.ts
@@ -0,0 +1,3 @@
+export type UploadRequestModel = {
+  file: File;
+}
