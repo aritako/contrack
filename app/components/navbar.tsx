014b0975e161c51f29e419497537dbcca024e91e
Carmelo Atienza <atienzabusiness@gmail.com>
2024-11-22 23:31:18 +0800

    feat: Legiscan -> Contrack

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link"
 
-export default function Navbar(){
+export default function Navbar() {
   return (
     <>
-      <Link href="/" className="young-serif text-3xl font-bold mb-6">Legiscan</Link>
+      <Link href="/" className="young-serif text-3xl font-bold mb-6">Contrack</Link>
     </>
   )
 
