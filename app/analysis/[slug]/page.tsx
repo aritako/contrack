ed6047cb6fed42c70ad8961801d940612c167f88
Carmelo Atienza <atienzabusiness@gmail.com>
2024-11-24 18:19:54 +0800

    feat(analysis): show pdf-image display

--- app/analysis/[slug]/page.tsx
+++ app/analysis/[slug]/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Navbar from "@/app/components/navbar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import {
@@ -8,13 +9,16 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { useEffect, useState } from "react";
+import { ComparisonDocument, FileMetadata } from "@/models/comparison";
+import { useParams } from "next/navigation";
 
-export default async function Page({
+export default function Page({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  const slug = (await params).slug;
+  const { slug } = useParams();
   const analysisContent = `
   <p>This document has been analyzed for potential conflicts with Philippine laws. Here are some insights:</p>
   <ul>
@@ -23,20 +27,57 @@ export default async function Page({
     <li><strong>Section 3:</strong> Analysis of terms and clauses in light of recent Supreme Court rulings.</li>
   </ul>
   `;
+  const [comparisonData, setComparisonData] = useState<ComparisonDocument>();
+  useEffect(() => {
+    if (!slug) {
+      return;
+    }
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`/api/documents?comparison_id=${slug}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch comparison data');
+        }
+        const data = await response.json();
+        console.log("DATA RETRIEVED", data);
+        setComparisonData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [slug]);
+
   return (
     <div className="flex flex-col p-8">
       <div className="grid lg:grid-cols-2">
         {/* Left side: PDF Viewer */}
         <div className="h-screen pl-8 pr-8 pb-8">
-          <iframe
-            src={`${process.env.AWS_S3_ENDPOINT_URL}/${slug}`}
-            width="100%"
-            height="100%"
-            title="Uploaded PDF"
-            className=""
-          ></iframe>
+          {comparisonData?.pdf.key ? (
+            <iframe
+              src={`${process.env.NEXT_PUBLIC_AWS_S3_ENDPOINT_URL}/${comparisonData.pdf.key}`}
+              width="100%"
+              height="100%"
+              title="Uploaded PDF"
+              className=""
+            ></iframe>
+          ) : (
+            <p>Loading PDF...</p>
+          )}
         </div>
-
+        <div className="h-screen pl-8 pr-8 pb-8">
+          {comparisonData?.pdf.key ? (
+            <iframe
+              src={`${process.env.NEXT_PUBLIC_AWS_S3_ENDPOINT_URL}/${comparisonData.image.key}`}
+              width="100%"
+              height="100%"
+              title="Uploaded Image"
+              className=""
+            ></iframe>
+          ) : (
+            <p>Loading Image...</p>
+          )}
+        </div>
         {/* Right side: Analysis */}
         <div className="pl-4">
           <Tabs defaultValue="Relevant Laws" className="w-full">
