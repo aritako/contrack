84a9988402dbe33f3dd70eaba5956355634e72d1
Carmelo Atienza <atienzabusiness@gmail.com>
2024-11-24 17:18:11 +0800

    feat: GET documents

--- app/api/documents/route.ts
+++ app/api/documents/route.ts
@@ -1,16 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { ComparisonModel } from '@/models/comparison';
+import { ComparisonDocument, ComparisonModel } from '@/models/comparison';
 import db_connection from '@/lib/db';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   try {
     await db_connection
-    const result = await ComparisonModel.find({})
-    console.log('Result:', result);
-    return NextResponse.json(result, { status: 200 });
+    const comparison_id = request.nextUrl.searchParams.get('comparison_id');
+    let result;
+    if (comparison_id) {
+      result = await ComparisonModel.findOne({ comparison_id: comparison_id });
+    } else {
+      result = await ComparisonModel.find({});
+    }
+    if (!result) {
+      return NextResponse.json({ error: 'Comparison not found' }, { status: 404, headers: { 'Content-Type': 'application/json' } });
+    }
+    return NextResponse.json(result, { status: 200, headers: { 'Content-Type': 'application/json' } });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    return NextResponse.json({ error: `Internal Server Error: ${error}` }, { status: 500 });
   }
 }
 
@@ -18,7 +26,7 @@ export async function GET() {
 export async function POST(request: NextRequest) {
   try {
     await db_connection;
-    const body = await request.json();
+    const body: ComparisonDocument = await request.json();
     const comparison = new ComparisonModel({
       comparison_id: body.comparison_id,
       user_id: body.user_id,
